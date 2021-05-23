b3f415893095c7e35876473a396953d9613b9f07
DANG VAN HAI <mmrhai2000@gmail.com>
2021-05-23 23:23:40 +0700

    23/5/2021 "Chỉnh sửa về back end"

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,20 @@
 import 'react'
 
 
+const button = () => {
+    return (
+      <div className="footer-container">
+        <section className="social-media">
+          <div className="social-media-wrap">
+            <div className="footer-logo">
+            
+            </div>
+            <small className="website-rights">AseanWeather - Nhóm 123 hihi</small>
+            <div className="">
+                <img className="logApiWeather" src="/assets/images/logoAPIWeather.png" alt="Logo api weather" />
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };

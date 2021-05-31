fb575439930ef42cab57b9fe97c62d4a1d1a81fa
NguyenTrongDuc256 <ducnguyentrong19@gmail.com>
2021-05-31 10:05:20 +0700

    Duc: add  ->searchbg/index.tsx  ->searchbg/searchbg.scss

--- src/components/searchbg/index.tsx
+++ src/components/searchbg/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./searchbg.scss";
 import "../../App.scss";
 import {
-  getWeatherSearchRequest,
   getWeatherNowRequest,
 } from "../../redux/effects/weatherEffects";
 import FavouriteLocation from "../favourite-location";
@@ -10,54 +9,88 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from 'axios';
+// import { isBuffer } from "node:util";
+import {useCookies}  from "react-cookie";
+import {listCity} from '../../data/ListCity';
 
 interface ISearch {
   propsData: any;
-  getWeatherSearchRequest: (searchKey: string) => void;
+  // getWeatherSearchRequest: (searchKey: string) => void;
   getWeatherNowRequest: (city?: string) => void;
+}
+const  xoa_dau=(str:string)=> {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+  str = str.replace(/Đ/g, "D");
+  return str;
 }
 
 const SearchComponent: React.FC<ISearch> = ({
-  propsData,
-  getWeatherSearchRequest,
-  getWeatherNowRequest,
+  getWeatherNowRequest,propsData
 }) => {
-  const [cityMatch, setCityMatch] = useState([]);
+  //Khai báo state để sử dụng
+  const [cityMatch, setCityMatch]:any[] = useState([]);
   const [text, setText] = useState("");
   const [show, setShow] = useState(false);
-  const [listCities, setlistCities] = useState([]);
-  const url = "https://vti-aca-april-team1-api.herokuapp.com/api/v1/cities";
-  const config = {
-    url,
-    headers: {
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Headers': 'Content-Type'
-      }
-  }
-  useEffect(() => {
-    const loadCities = async () => {
-      const response = await axios(config);
-      setlistCities(response.data);
-    };
-    loadCities();
-},[])
+  const [listCities, setlistCities] = useState(listCity);
+  const [cookies, setCookie] = useCookies(['ipAddress']);
 
-  const handleSearch = (text: string) => {
-    // e.preventDefault()
-    // if (!text) {
-    //   setCityMatch([]);
-    //   setShow(false);
-    // } else {
-    //   setShow(true);
-    //   getWeatherSearchRequest(text.trim());
-    //   setCityMatch(propsData.location);
-    // }
-    // setText(text);
+  //Config api tìm kiếm
+  // const url = "https://api-weather-asean.herokuapp.com/api/v1/cities";
+  // const config = {
+  //   url,
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  //     'Access-Control-Allow-Credentials': true,
+  //     'Access-Control-Allow-Headers': 'Content-Type'
+  //   }
+  // }
+
+  const getTotalView = ()=>{
+        if(!cookies.ipAddress)
+        {
+          axios.put(
+              `https://api-weather-asean.herokuapp.com/api/v1/ip`
+            ).then((res)=>{
+              localStorage.setItem("count",res.data.data.count);
+              setCookie('ipAddress', res.data.data.ip ,{maxAge : 900});
+            })
+        }else{
+          axios.get(
+            `https://api-weather-asean.herokuapp.com/api/v1/count`
+          ).then((res)=>{
+            localStorage.setItem("count",res.data.data.count);
+          })
+        }
+  }
+  //Load api tìm kiếm địa phương
+  useEffect(() => {
+    // const loadCities = async () => {
+    //   const response = await axios(config);
+    //   setlistCities(response.data);
+    // };
+    getTotalView(); 
+    // loadCities();
+    
+  }, [])
+
+  //Match input with list city
+  const handleSearch = async(text: string) => {
     if (!text) {
-      setCityMatch([]); 
-      setShow(false);  
+      setCityMatch([]);
+      setShow(false);
     } else {
       setShow(true);
       let matches = listCities.filter((city: any) => {
@@ -66,29 +99,34 @@ const SearchComponent: React.FC<ISearch> = ({
       });
       setCityMatch(matches);
     }
-    setText(text);
+     await setText(text);
   };
+  
+  //Click item filter
   const onCityHandler = (item: any) => {
     setText(item.name);
-    getWeatherNowRequest(item.lable);
+    if(propsData.nowloading){
+      getWeatherNowRequest(item.lable);
+    }
     setCityMatch([]);
   };
-  const searchClick=()=>{
-    console.log("text",text);
-    getWeatherNowRequest(text);
-  };
+
 
   return (
     <div className="hero-container">
+
+      {/* Giao diện của trang search */}
       <video src="/assets/videos/video-1.mp4" autoPlay loop muted />
-      <h3>Asean Weather</h3>
-      <h5>Today , What is the weather like in your place ? </h5>
+      <h3 className="text-home-title-response">Asean Weather</h3>
+      <h5 className="text-home-detail-response">Today , What is the weather like in your place ? </h5>
+
+      {/* Thanh search */}
       <div className="hero-btns">
-        <div className="d-flex ">
+        <div className="d-flex wrap-input-response">
           <input
             type="text"
             className="form-control input_search"
-            placeholder="&#xF002; Search Location..."
+            placeholder="&#xF002; Search location..."
             onChange={(e) => handleSearch(e.target.value)}
             value={text}
             onBlur={() => {
@@ -98,44 +136,45 @@ const SearchComponent: React.FC<ISearch> = ({
               }, 200);
             }}
           />
-           <Link to="/now">
-            <button className="btn-search" onClick={searchClick}>
-              <i className="fas fa-search icon-search"></i>
-            </button> 
-           </Link>
-          
+          <Link to={`now/${xoa_dau(text)}`} >
+            <button className="btn-search" style={{ backgroundColor: show ? "white" : "#1e90ff" }} >
+              <i className="fas fa-search icon-search" style={{ color: show ? "#747d8c" : "#dcdde1" }}></i>
+            </button>
+          </Link>
         </div>
-        <br />
-
-        <FavouriteLocation />
-
         <div
-          className="suggest-wrap"
+          className="suggest-wrap" 
           style={{ display: show ? "block" : "none" }}
         >
-          {/* {listCities.map((item: any, index: any) => {
-            return (
-              <Link to="/now" key={index}>
-                <div className="suggest" onClick={() => onCityHandler(item)}>
-                  {item.name}, {item.country.name}
-                </div>
-              </Link>
-            );
-          })} */}
+          {
+            cityMatch.length === 0 && (<div className="suggest text-danger" id="suggest">Không tìm thấy kết quả nào .</div>)
+          }
           {cityMatch &&
             cityMatch.map((item: any, index: any) => (
-              <Link to="/now" key={index}>
-              <div
-                className="suggest"
-                onClick={() => onCityHandler(item)}
-              >
-                {item.name}
-              </div>
+              <Link to={`/now/${item.lable}`} key={index}>
+                <div
+                  className="suggest"
+                  onClick={() => onCityHandler(item)}
+                >
+                  <i className="fas fa-search search-item-icon"></i>
+                  {item.name}
+                </div>
               </Link>
             ))}
         </div>
+        <br />
+
+        {/* Hiển thị 3 địa phương đầu trong danh sách yêu thích */}
+        <FavouriteLocation />
+      </div>
+
+      {/* view số người xem trang web từ trước tới giờ */}
+      <div className="view-fixed-panel">
+        <i className="fas fa-eye"></i>
+        <span>{localStorage.getItem("count")}</span>              
       </div>
     </div>
+
   );
 };
 
@@ -147,7 +186,7 @@ const mapStateToProps = (state: any) => {
 const mapDispatchToProps = (dispatch: any) =>
   bindActionCreators(
     {
-      getWeatherSearchRequest,
+      // getWeatherSearchRequest,
       getWeatherNowRequest,
     },
     dispatch
